551632ad7699d2725e654613304356b5b55373ff
Hangi Chon <hanpencnd@gmail.com>
2022-02-01 10:19:17 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+REST API - CRUD Operation using texting acronyms/slangs data populated in mongoDB
---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>sdf</div>;
+};
+
+export default App;
