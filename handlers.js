1eabbf4dd910d1b933c81dae9bc5b21c0bb5e570
Hangi Chon <hanpencnd@gmail.com>
2022-02-02 05:07:24 -0500

    Addition of Patch and Delete request for acronyms

--- handlers.js
+++ handlers.js
@@ -1,18 +1,57 @@
-const { Acronym } = require("./seeds");
+// bring URI from .env file
+require("dotenv").config();
+
 const { v4: uuidv4 } = require("uuid");
+const { MONGO_URI } = process.env;
+const mongoose = require("mongoose");
+const { Acronym, injectData } = require("./seeds");
+
+// connect to mongoDB using mongoose
+const activateConn = async () => {
+  await mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  await mongoose.connection.on("connected", () => {
+    console.log("Mongoose is connected!");
+  });
+};
+
+// verify model existence to avoid redundant seeding
+mongoose.connection.once("open", async () => {
+  if ((await Acronym.countDocuments().exec()) > 0) {
+    // console.log("Acronyms already exist in database.");
+    return;
+  } else {
+    injectData();
+    console.log("Acronyms added for the first time.");
+  }
+});
+
+// disconnect mongoose
+const deactivateConn = async () => {
+  await mongoose.disconnect();
+  console.log("Mongoose is disconnected!");
+};
 
 // response template
 const response = (res, code, msg, data) => {
   return res.status(code).json({ status: code, data: data, message: msg });
 };
 
+// initial connection required to inject data
+activateConn();
+
 //************************************************//
-//   GET /acronym?page=1&limit=10&serach=:search  //
+//   GET /acronym?page=1&limit=10&search=:search  //
 //************************************************//
-const getAcronyms = (req, res) => {
+const getAcronyms = async (req, res) => {
   const page = parseInt(req.query.page);
   const limit = parseInt(req.query.limit);
   const { search } = req.query;
+  let results = {};
+  await activateConn();
 
   // patterns used to match character combinations in strings
   const escapeRegex = text => {
@@ -20,62 +59,105 @@ const getAcronyms = (req, res) => {
   };
 
   try {
-    // pagination info to be used in response
-    const startIdx = (page - 1) * limit;
-    const endIdx = page * limit;
-    const results = {};
-
-    // formulate returned result from db
-    const formulateFoundResult = foundResult => {
-      // consistent logic for showing previous page
-      if (startIdx > 0) {
-        results.previous = {
-          page: page - 1,
-          limit: limit
-        };
-      }
-      // results.next depends on fetched result
-      if (endIdx < foundResult.length) {
-        results.next = {
-          page: page + 1,
-          limit: limit
-        };
-      }
-      results.total = foundResult.length;
-      results.result = foundResult.slice(startIdx, endIdx);
-
-      return results;
-    };
-
     // fuzzy or full search
     if (search) {
       // apply RegExp for fuzzy search (to be improved by using mongoDB's fuzzy query method)
       const regex = new RegExp(escapeRegex(search), "gi");
 
+      // get total result quantity
+      const resultLength = (await Acronym.find({ "acronym": regex })).length;
+
       // find matched strings and formulate response accordingly
-      Acronym.find({ "acronym": regex }, (err, foundAcronyms) => {
-        foundAcronyms.length
-          ? response(
-              res,
-              200,
-              "Successfully retrieved acronym(s)",
-              formulateFoundResult(foundAcronyms)
-            )
-          : response(res, 400, "No matching acronyms found");
-      });
+      Acronym.find({ "acronym": regex })
+        .limit(limit)
+        .skip(page * limit)
+        .exec(async (err, matchedAcronyms) => {
+          results = {
+            total: resultLength,
+            result: matchedAcronyms,
+            // show available result quantity in previous and next page
+            // if next page only has X number of results, the limit will appear to be X
+            // These admittedly messy looking ternary operators can be improved
+            // by implementing mongoose built-in methods in the future
+            previous:
+              page > 0
+                ? limit * (page - 1) < resultLength
+                  ? {
+                      page: page - 1,
+                      limit:
+                        resultLength - limit * (page - 1) < limit
+                          ? resultLength - limit * (page - 1)
+                          : limit
+                    }
+                  : {}
+                : {},
+            next:
+              (page + 1) * limit < resultLength
+                ? {
+                    page: page + 1,
+                    limit:
+                      limit * (page + 2) < resultLength
+                        ? limit
+                        : limit * (page + 1) < resultLength
+                        ? resultLength % (limit * (page + 1))
+                        : {}
+                  }
+                : {}
+          };
+
+          if (err) {
+            response(res, 400, "Something went wrong", err);
+          } else {
+            response(res, 200, "Successfully retrieved all acronyms", results);
+          }
+          await deactivateConn();
+        });
 
       // no search query
     } else {
-      Acronym.find({}, (err, allAcronyms) => {
-        allAcronyms.length
-          ? response(
-              res,
-              200,
-              "Successfully retrieved all acronyms",
-              formulateFoundResult(allAcronyms)
-            )
-          : response(res, 400, "Something went wrong");
-      });
+      // get total result quantity
+      const resultLength = (await Acronym.find({})).length;
+
+      Acronym.find({})
+        .limit(limit)
+        .skip(page * limit)
+        .exec(async (err, allAcronyms) => {
+          results = {
+            total: resultLength,
+            result: allAcronyms,
+            previous:
+              page > 0
+                ? limit * (page - 1) < resultLength
+                  ? {
+                      page: page - 1,
+                      limit:
+                        resultLength - limit * (page - 1) < limit
+                          ? resultLength - limit * (page - 1)
+                          : limit
+                    }
+                  : {}
+                : {},
+            next:
+              (page + 1) * limit < resultLength
+                ? {
+                    page: page + 1,
+                    limit:
+                      limit * (page + 2) < resultLength
+                        ? limit
+                        : limit * (page + 1) < resultLength
+                        ? resultLength % (limit * (page + 1))
+                        : {}
+                  }
+                : {}
+          };
+
+          if (err) {
+            response(res, 400, "Something went wrong", err);
+          } else {
+            response(res, 200, "Successfully retrieved all acronyms", results);
+          }
+          await deactivateConn();
+        });
     }
   } catch (error) {
     return response(res, 500, "Server Error", error);
@@ -85,18 +167,28 @@ const getAcronyms = (req, res) => {
 //************************************************//
 //                 POST /acronym                  //
 //************************************************//
-const addAcronym = (req, res) => {
+const addAcronym = async (req, res) => {
   const { acronym, definition } = req.body;
 
   try {
-    Acronym.create({ _id: uuidv4(), acronym, definition }, (err, result) => {
-      result
-        ? response(res, 200, "Successfully added a texting abbreviation", {
+    await activateConn();
+
+    Acronym.create(
+      { _id: uuidv4(), acronym, definition },
+      async (err, result) => {
+        const { acronym, definition } = result;
+
+        if (err) {
+          response(res, 400, "Something went wrong", err);
+        } else {
+          response(res, 200, "Successfully added a texting abbreviation", {
             acronym,
             definition
-          })
-        : response(res, 400, "Something went wrong");
-    });
+          });
+        }
+        await deactivateConn();
+      }
+    );
   } catch (error) {
     response(res, 500, "Server Error", error);
   }
@@ -105,11 +197,61 @@ const addAcronym = (req, res) => {
 //************************************************//
 //            PATCH /acronym/:acronymID           //
 //************************************************//
-const updateAcronym = (req, res) => {};
+const updateAcronym = async (req, res) => {
+  const { acronymID } = req.params;
+  const { acronym: newAcronym, definition: newDefinition } = req.body;
+
+  try {
+    await activateConn();
+
+    Acronym.findOneAndUpdate(
+      { _id: acronymID },
+      { acronym: newAcronym, definition: newDefinition },
+      async (err, result) => {
+        const { acronym, definition } = result;
+
+        if (err) {
+          response(res, 400, "Something went wrong", err);
+        } else {
+          response(res, 200, "Successfully updated a texting abbreviation", {
+            acronym: acronym + "->" + newAcronym,
+            definition: definition + "->" + newDefinition
+          });
+
+          await deactivateConn();
+        }
+      }
+    );
+  } catch (error) {
+    response(res, 500, "Server Error", error);
+  }
+};
 
 //************************************************//
 //           DELETE /acronym/:acronymID           //
 //************************************************//
-const deleteAcronym = async (req, res) => {};
+const deleteAcronym = async (req, res) => {
+  const { acronymID } = req.params;
+
+  try {
+    await activateConn();
+
+    Acronym.findOneAndDelete({ _id: acronymID }, async (err, result) => {
+      const { acronym, definition } = result;
+
+      if (err) {
+        response(res, 400, "Something went wrong", err);
+      } else {
+        response(res, 200, "Successfully deleted the acronym", {
+          acronym,
+          definition
+        });
+      }
+      await deactivateConn();
+    });
+  } catch (error) {
+    response(res, 500, "Server Error", error);
+  }
+};
 
 module.exports = { getAcronyms, addAcronym, updateAcronym, deleteAcronym };
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// bring URI from .env file
-require("dotenv").config();
+// // bring URI from .env file
+// require("dotenv").config();
 
 const express = require("express");
-const { MONGO_URI } = process.env;
+// const { MONGO_URI } = process.env;
 const mongoose = require("mongoose");
 
 // utils and handlers
-const { Acronym, injectData } = require("./seeds");
+// const { Acronym, injectData } = require("./seeds");
 const {
   getAcronyms,
   addAcronym,
@@ -20,28 +20,28 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.static("public"));
 
-// connect to mongoDB using mongoose
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// // connect to mongoDB using mongoose
+// mongoose.connect(MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
-mongoose.connection.on("connected", () => {
-  console.log("Mongoose is connected!!!!");
-});
+// mongoose.connection.on("connected", () => {
+//   console.log("Mongoose is connected!");
+// });
 
-// verify model existence to avoid redundant seeding
-mongoose.connection.once("open", async () => {
-  if ((await Acronym.countDocuments().exec()) > 0) {
-    console.log("Acronyms already exist in database.");
-  } else {
-    injectData();
-    console.log("Acronyms added for the first time.");
-  }
-});
+// // verify model existence to avoid redundant seeding
+// mongoose.connection.once("open", async () => {
+//   if ((await Acronym.countDocuments().exec()) > 0) {
+//     console.log("Acronyms already exist in database.");
+//   } else {
+//     injectData();
+//     console.log("Acronyms added for the first time.");
+//   }
+// });
 
 //************************************************//
-//   GET /acronym?page=1&limit=10&serach=:search  //
+//   GET /acronym?page=1&limit=10&search=:search  //
 //************************************************//
 app.get("/acronym", getAcronyms);
 
